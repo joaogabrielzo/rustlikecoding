20036dab79a0bded40eb67b45673c98a4e6c2d77
João Gabriel Zó <jgzolanes@gmail.com>
2023-05-29 06:57:01 -0300

    init

--- rendering/src/main.rs
+++ rendering/src/main.rs
@@ -1,15 +1,9 @@
-mod transformations;
-
 use bevy::prelude::*;
 use bevy::window::PresentMode;
 use bevy_inspector_egui::quick::WorldInspectorPlugin;
-use transformations::*;
 
 fn main() {
     App::new()
-        .register_type::<Translation>()
-        .register_type::<Scaling>()
-        .register_type::<Rotation>()
         .insert_resource(ClearColor(Color::rgb(0.1, 0.1, 0.1)))
         .add_startup_system(spawn_basic_scene)
         .add_plugins(DefaultPlugins.set(WindowPlugin {
@@ -23,7 +17,6 @@ fn main() {
         // .add_plugin(bevy::diagnostic::LogDiagnosticsPlugin::defaultMathFunction())
         // .add_plugin(bevy::diagnostic::FrameTimeDiagnosticsPlugin::default())
         .add_plugin(WorldInspectorPlugin::new())
-        .add_system(update_transformations_system)
         .run();
 }
 
@@ -34,10 +27,6 @@ fn spawn_basic_scene(
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
-    commands.spawn(Translation::default());
-    commands.spawn(Scaling::default());
-    commands.spawn(Rotation::default());
-
     for z in 0..GRID_RESOLUTION {
         for y in 0..GRID_RESOLUTION {
             for x in 0..GRID_RESOLUTION {
@@ -94,25 +83,3 @@ fn get_coordinates(x: i32, y: i32, z: i32) -> Vec3 {
 
 #[derive(Component)]
 struct Shape;
-
-fn update_transformations_system(
-    _translation_query: Query<&Translation, Changed<Translation>>,
-    _scaling_query: Query<&Scaling, Changed<Scaling>>,
-    rotation_query: Query<&Rotation, Changed<Rotation>>,
-    mut transform_query: Query<&mut Transform, With<Shape>>,
-) {
-    if let Ok(rotation) = rotation_query.get_single() {
-        for mut transform in &mut transform_query {
-            let rad_z = rotation.v.z.to_radians();
-            let sin_z = rad_z.sin();
-            let cos_z = rad_z.cos();
-
-            transform.rotation = Quat::from_xyzw(
-                transform.rotation.x * cos_z - transform.rotation.y * sin_z,
-                transform.rotation.x * sin_z - transform.rotation.y * cos_z,
-                rad_z,
-                1.0,
-            );
-        }
-    }
-}
